05082c11f0f85731427585e54f0a6a9bb7fea2a4
Michael Hadley <michaelwesthadley@gmail.com>
2018-12-02 10:27:05 -0600

    Gatsby config based on blog starter

---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  siteMetadata: {
+    title: "Sporadic Labs",
+    siteUrl: "https://sporadic-labs.github.io/"
+  },
+  pathPrefix: ".",
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages"
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
+          },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants"
+        ]
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-116363382-1`
+      }
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Sporadic Labs`,
+        short_name: `Sporadic Labs`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icon: `src/assets/logo.png`
+      }
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`
+  ]
+};
