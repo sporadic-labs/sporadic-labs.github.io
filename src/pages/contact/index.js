c439e1c1ff5f93392b33b433565aafbab61a5389
rex <retwedt@gmail.com>
2018-12-09 10:36:20 -0600

    Validate the contact form before submission.  Show a useful message if the user tries to submit an empty form.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,35 +1,88 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import PageLayout from "../../components/page-layout";
 import style from "./index.module.styl";
 
-export default () => (
-  <PageLayout title="Contact">
-    <h1>Get in Touch</h1>
-    <p>Interested in our work? Or, maybe you just want to say hello? Drop us a line.</p>
-    <form className={style.form} action="https://formspree.io/sporadiclabs@gmail.com" method="POST">
-      <div className={style.field}>
-        <label className={style.label} htmlFor="contact-name">
-          Your Name
-        </label>
-        <input className={style.control} type="text" id="contact-name" name="name" />
-      </div>
-      <div className={style.field}>
-        <label className={style.label} htmlFor="contact-email">
-          Your Email
-        </label>
-        <input className={style.control} type="email" id="contact-email" name="email" />
-      </div>
-      <div className={style.field}>
-        <label className={style.label} htmlFor="contact-message">
-          Your Message
-        </label>
-        <textarea className={style.control} name="message" rows="4" id="contact-message" />
-      </div>
-      <div className={style.field}>
-        <input className={style.submitBtn} type="submit" value="Send →" />
-      </div>
-      <input type="text" name="_gotcha" style={{ display: "none" }} />
-      <input type="hidden" name="_subject" value="Sporadic Labs Contact" />
-    </form>
-  </PageLayout>
-);
+const defined = val => {
+  return val !== undefined && val !== null && val !== "";
+};
+
+class Contact extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.validate = this.validate.bind(this);
+    this.contactName = React.createRef();
+    this.contactEmail = React.createRef();
+    this.contactMessage = React.createRef();
+  }
+
+  validate(event) {
+    if (
+      !defined(this.contactName.current.value) ||
+      !defined(this.contactEmail.current.value) ||
+      !defined(this.contactMessage.current.value)
+    ) {
+      alert("Hey send a message or something!");
+      event.preventDefault();
+    }
+  }
+
+  render() {
+    return (
+      <PageLayout title="Contact">
+        <h1>Get in Touch</h1>
+        <p>Interested in our work? Or, maybe you just want to say hello? Drop us a line.</p>
+        <form
+          name="sl-contact-form"
+          className={style.form}
+          action="https://formspree.io/sporadiclabs@gmail.com"
+          onSubmit={this.validate}
+          method="POST"
+        >
+          <div className={style.field}>
+            <label className={style.label} htmlFor="contact-name">
+              Your Name
+            </label>
+            <input
+              className={style.control}
+              ref={this.contactName}
+              type="text"
+              id="contact-name"
+              name="name"
+            />
+          </div>
+          <div className={style.field}>
+            <label className={style.label} htmlFor="contact-email">
+              Your Email
+            </label>
+            <input
+              className={style.control}
+              ref={this.contactEmail}
+              type="email"
+              id="contact-email"
+              name="email"
+            />
+          </div>
+          <div className={style.field}>
+            <label className={style.label} htmlFor="contact-message">
+              Your Message
+            </label>
+            <textarea
+              className={style.control}
+              ref={this.contactMessage}
+              name="message"
+              rows="4"
+              id="contact-message"
+            />
+          </div>
+          <div className={style.field}>
+            <input className={style.submitBtn} type="submit" value="Send →" />
+          </div>
+          <input type="text" name="_gotcha" style={{ display: "none" }} />
+          <input type="hidden" name="_subject" value="Sporadic Labs Contact" />
+        </form>
+      </PageLayout>
+    );
+  }
+}
+
+export default Contact;
