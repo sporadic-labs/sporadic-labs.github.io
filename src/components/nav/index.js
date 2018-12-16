eaf5af39c0c88a3d36c3c08548e804d120779f34
Michael Hadley <michaelwesthadley@gmail.com>
2018-12-16 12:07:06 -0600

    Restructure nav to display a collapsed nav on small devices

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,8 +2,61 @@ import React, { PureComponent } from "react";
 import { Link } from "gatsby";
 import style from "./index.module.styl";
 import Container from "../container";
+import menuIconSrc from "../../assets/menu-icon.svg";
+import closeIconSrc from "../../assets/close-menu-icon.svg";
+import resizeEvent from "../../utils/resize-event";
 
-class Nav extends PureComponent {
+const links = [
+  <Link key="games" className={style.link} activeClassName={style.activeLink} to="/">
+    Games
+  </Link>,
+  <Link key="tools" className={style.link} activeClassName={style.activeLink} to="/tools">
+    Tools
+  </Link>,
+  <Link key="about" className={style.link} activeClassName={style.activeLink} to="/about">
+    About
+  </Link>,
+  <Link key="contact" className={style.link} activeClassName={style.activeLink} to="/contact">
+    Contact
+  </Link>
+];
+
+class CollapsedNav extends PureComponent {
+  state = {
+    isOpen: false
+  };
+
+  openMenu = () => this.setState({ isOpen: true });
+  closeMenu = () => this.setState({ isOpen: false });
+
+  render() {
+    const { isOpen } = this.state;
+
+    return (
+      <Container>
+        <nav className={style.collapsedNav}>
+          <div className={style.logo}>
+            <Link to="/">Sporadic Labs</Link>
+          </div>
+          <button disabled={isOpen} className={style.openButton}>
+            <img src={menuIconSrc} alt="Open Menu" onClick={this.openMenu} role="button" />
+          </button>
+        </nav>
+        {isOpen && (
+          <div className={style.collapsedOverlayMenu}>
+            <button className={style.closeButton}>
+              <img src={closeIconSrc} alt="Close Menu" onClick={this.closeMenu} role="button" />
+            </button>
+            <div className={style.overlay} />
+            <div className={style.overlayLinks}>{links}</div>
+          </div>
+        )}
+      </Container>
+    );
+  }
+}
+
+class ExpandedNav extends PureComponent {
   render() {
     return (
       <Container>
@@ -11,23 +64,33 @@ class Nav extends PureComponent {
           <div className={style.logo}>
             <Link to="/">Sporadic Labs</Link>
           </div>
-          <div>
-            <Link className={style.link} activeClassName={style.activeLink} to="/">
-              Games
-            </Link>
-            <Link className={style.link} activeClassName={style.activeLink} to="/tools">
-              Tools
-            </Link>
-            <Link className={style.link} activeClassName={style.activeLink} to="/about">
-              About
-            </Link>
-            <Link className={style.link} activeClassName={style.activeLink} to="/contact">
-              Contact
-            </Link>
-          </div>
+          <div>{links}</div>
         </nav>
       </Container>
     );
+  }
+}
+
+class Nav extends PureComponent {
+  state = {
+    width: window.innerWidth
+  };
+
+  componentDidMount() {
+    resizeEvent.addListener(this.onResize);
+  }
+
+  componentWillUnmount() {
+    resizeEvent.removeListener(this.onResize);
+  }
+
+  onResize = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+  render() {
+    const { width } = this.state;
+    return width > 700 ? <ExpandedNav /> : <CollapsedNav />;
   }
 }
 
